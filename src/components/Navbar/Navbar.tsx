ea3d42083cf7eb103108f26e20569704a5bceabd
Abu Naim <abunaim140@gmail.com>
2025-01-10 01:37:19 +0600

    added navbar icons with badge

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,17 @@
+import { IoSearchOutline } from 'react-icons/io5';
 import NavLink from './NavLink';
 import { navLinks } from './navLinks';
+import Link from 'next/link';
+import { FiUser } from 'react-icons/fi';
+import { IoMdHeartEmpty } from 'react-icons/io';
+import { PiShoppingCartSimple } from 'react-icons/pi';
 
 const Navbar = () => {
   return (
-    <nav className="bg-transparent h-fit py-8 border-b-[1px]">
-      <div className="container flex items-center">
+    <nav className='bg-transparent h-fit py-8 border-b-[1px]'>
+      <div className='container flex items-center'>
         <div className='flex items-center gap-2 flex-1'>
-          <div className="logo flex-1 text-2xl md:text-3xl font-semibold">
+          <div className='logo flex-1 text-2xl md:text-3xl font-semibold'>
             drapeGear
           </div>
         </div>
@@ -19,8 +24,17 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div className="flex items-center justify-end gap-4 flex-1">
-          <button className=''>Login</button>
+        <div className='flex items-center justify-end gap-2 md:gap-4 text-[22px] md:text-2xl flex-1'>
+          <IoSearchOutline className='cursor-pointer transition-all duration-300 hover:text-cyan-500' />
+          <Link href='/login'><FiUser className='hidden md:block cursor-pointer transition-all duration-300 hover:text-cyan-500' /></Link>
+          <div className='relative hidden md:block cursor-pointer group'>
+            <IoMdHeartEmpty className='transition-all duration-300 group-hover:text-cyan-500' />
+            <div className='absolute -top-1 -right-1.5 min-w-4 min-h-4 rounded-full flex items-center justify-center text-[10px] leading-none text-white bg-black'>1</div>
+          </div>
+          <div className='relative hidden md:block cursor-pointer group'>
+            <PiShoppingCartSimple className='transition-all duration-300 group-hover:text-cyan-500' />
+            <div className='absolute -top-1 -right-1.5 min-w-4 min-h-4 rounded-full flex items-center justify-center text-[10px] leading-none text-white bg-black'>1</div>
+          </div>
         </div>
       </div>
     </nav>
