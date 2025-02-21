6ae0b7730868a930f7c78c5b77314bb8013e7560
Abu Naim <abunaim140@gmail.com>
2025-02-22 00:56:46 +0600

    added users page and display the users data in a table

---
+++ src/app/users/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+import { useAppSelector } from "@/lib/hooks";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
+import DashboardNav from "@/components/DashboardNav/DashboardNav";
+import { PiTrashLight } from "react-icons/pi";
+
+const Users = () => {
+    const { users } = useAppSelector((state) => state.users);
+
+    return (
+        <>
+            <Breadcrumb />
+            <div className='container min-h-[calc(100vh-412px)] mb-20'>
+                <div className='flex flex-col md:flex-row gap-8 mt-16'>
+                    <DashboardNav />
+                    <Table className='border'>
+                        <TableCaption>A list of your recent users.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">Name</TableHead>
+                                <TableHead>Email</TableHead>
+                                <TableHead>Date</TableHead>
+                                <TableHead>Role</TableHead>
+                                <TableHead className="text-right">Manage</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {users.map((user) => (
+                                <TableRow key={user._id}>
+                                    <TableCell className="font-medium">{user.name}</TableCell>
+                                    <TableCell>{user.email}</TableCell>
+                                    <TableCell>{user.createdAt}</TableCell>
+                                    <TableCell>{user.role}</TableCell>
+                                    <TableCell className="text-right py-1">
+                                        <button className={`text-center text-base px-5 py-2 transition-all duration-500 bg-black text-white hover:bg-gray-800 hover:text-red-500 ${user.role === 'admin' ? 'pointer-events-none bg-gray-500' : ''}`}><PiTrashLight /></button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Users;
