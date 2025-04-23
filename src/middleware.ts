1fe2d64ffe0271f855ccf9426c26f38e9401a3d8
Abu Naim <abunaim140@gmail.com>
2025-04-24 01:30:21 +0600

    modify and adjust middleware for role based access

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,39 @@
 import { NextResponse } from "next/server";
 import { auth } from "./auth";
 
+const roleProtectedRoutes: { path: string, role: string[] }[] = [
+    { path: '/account', role: ['admin', 'user'] },
+    { path: '/addresses', role: ['admin', 'user'] },
+    { path: '/allproducts', role: ['admin'] },
+    { path: '/addproduct', role: ['admin'] },
+    { path: '/cart', role: ['user'] },
+    { path: '/orders', role: ['admin', 'user'] },
+    { path: '/payment', role: ['user'] },
+    { path: '/users', role: ['admin'] }
+];
+
 export default auth((req) => {
     const currentPath = req.nextUrl.pathname;
+    const session = req.auth;
 
-    if (!req.auth) {
+    if (!session) {
         return NextResponse.redirect(
             new URL(`/login?next=${currentPath}`, req.url)
         );
+    };
+
+    const userRole = session?.user?.role;
+    const protectedRoute = roleProtectedRoutes.find(route => currentPath === route.path);
+
+    if (protectedRoute && !protectedRoute.role.includes(userRole)) {
+        return NextResponse.redirect(
+            new URL('/unauthorized', req.url)
+        )
     }
 
     return NextResponse.next();
 });
 
 export const config = {
-    matcher: ['/account', '/addresses', '/allproducts', '/addproduct', '/cart', '/orders', '/payment', '/users', ]
+    matcher: ['/account', '/addresses', '/allproducts', '/addproduct', '/cart', '/orders', '/payment', '/users',]
 };
